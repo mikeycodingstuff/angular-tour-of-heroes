e9e9d9138958edfce14bebcf9f3e7f760af19d64
Mikey Ying <mikeyinguk@gmail.com>
2023-01-31 17:21:31 +0000

    removed unused method and property

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,19 +11,12 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent {
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
-
-  selectedHero?: Hero;
-
   heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
