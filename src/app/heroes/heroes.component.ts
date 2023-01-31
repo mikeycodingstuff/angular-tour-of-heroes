bc52784c70b5b03fafff2881b07b97093c37ad1b
Mikey Ying <mikeyinguk@gmail.com>
2023-01-31 16:13:15 +0000

    refactored component to use subscribe to fetch data asynchronously

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,7 +19,8 @@ export class HeroesComponent {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
